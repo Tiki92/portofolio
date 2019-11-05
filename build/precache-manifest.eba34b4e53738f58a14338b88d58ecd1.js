178c160d2e55aa27fe658dd434fafe4745d040b0
Burca Vlad <tikiguy1642@gmail.com>
2019-11-05 12:41:28 +0200

    added iWish Flutter Project

---
+++ build/precache-manifest.eba34b4e53738f58a14338b88d58ecd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b8e5cfeb32d1792775",
+    "url": "/portofolio/static/css/main.3a3f7d32.chunk.css"
+  },
+  {
+    "revision": "21b8e5cfeb32d1792775",
+    "url": "/portofolio/static/js/main.21b8e5cf.chunk.js"
+  },
+  {
+    "revision": "e796f50c5212124e67ff",
+    "url": "/portofolio/static/js/runtime~main.e796f50c.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portofolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "57a861e912c6f01502a493050903cbdd",
+    "url": "/portofolio/index.html"
+  }
+];

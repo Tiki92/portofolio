9744b755ad327d940a96aea43b245f68bd98a6f2
vlad.burca <tikiguy1642@gmail.com>
2021-11-03 18:23:44 +0200

    Updates

---
+++ precache-manifest.fe87ddb4ee5d4c1658cbee692e923081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e796f50c5212124e67ff",
+    "url": "/portofolio/static/js/runtime~main.e796f50c.js"
+  },
+  {
+    "revision": "642648dbff664c17be0e",
+    "url": "/portofolio/static/js/main.642648db.chunk.js"
+  },
+  {
+    "revision": "1c3bccd22e4eec3e27da",
+    "url": "/portofolio/static/js/2.1c3bccd2.chunk.js"
+  },
+  {
+    "revision": "642648dbff664c17be0e",
+    "url": "/portofolio/static/css/main.d5bf8068.chunk.css"
+  },
+  {
+    "revision": "e10bf1ddc384422a8d6cad3259b7e26d",
+    "url": "/portofolio/index.html"
+  }
+];
